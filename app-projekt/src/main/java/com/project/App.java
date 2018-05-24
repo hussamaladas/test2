414aee315191970e2b30e6c1d56b9c481f97a262
ADESSO\aladas <Hussam.Aladas@adesso-insurance-solutions.de>
2018-05-24 11:31:24 +0200

    add

---
+++ app-projekt/src/main/java/com/project/App.java
@@ -0,0 +1,20 @@
+package com.project;
+
+public class App {
+	public String Sample(){
+		return "sample-New";
+		
+	}
+	
+//	public static Double addiere(Double a, Double b) {
+//		return a + b;
+//	}
+
+	public static int addiere(int a, int b) {
+		// TODO Auto-generated method stub
+		return a + b;
+	}
+
+	
+	
+}
---
+++ app-projekt/src/test/java/com/project/AppTest.java
@@ -0,0 +1,17 @@
+package com.project;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		assertEquals(9, App.addiere(4,5)); 
+	}
+
+	
+	
+
+}
